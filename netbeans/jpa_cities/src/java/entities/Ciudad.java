e2ace1d219ed71073def633c38e63f8e40e35d2d
Diego Fernández Criado <diegofercri25@gmail.com>
2024-12-09 14:11:31 +0100

    add netbeans and eclipse folders

---
+++ netbeans/jpa_cities/src/java/entities/Ciudad.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author micro
+ */
+@Entity
+@Table(name = "CIUDAD")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Ciudad.findAll", query = "SELECT c FROM Ciudad c")
+    , @NamedQuery(name = "Ciudad.findById", query = "SELECT c FROM Ciudad c WHERE c.id = :id")
+    , @NamedQuery(name = "Ciudad.findByNombre", query = "SELECT c FROM Ciudad c WHERE c.nombre = :nombre")
+    , @NamedQuery(name = "Ciudad.findByImagen", query = "SELECT c FROM Ciudad c WHERE c.imagen = :imagen")
+    , @NamedQuery(name = "Ciudad.findByDescripcion", query = "SELECT c FROM Ciudad c WHERE c.descripcion = :descripcion")
+    , @NamedQuery(name = "Ciudad.findByLink", query = "SELECT c FROM Ciudad c WHERE c.link = :link")
+    , @NamedQuery(name = "Ciudad.findByMapa", query = "SELECT c FROM Ciudad c WHERE c.mapa = :mapa")})
+public class Ciudad implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Short id;
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Column(name = "IMAGEN")
+    private String imagen;
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+    @Column(name = "LINK")
+    private String link;
+    @Column(name = "MAPA")
+    private String mapa;
+    @OneToMany(mappedBy = "ciudad")
+    private List<Ruta> rutaList;
+
+    public Ciudad() {
+    }
+
+    public Ciudad(Short id) {
+        this.id = id;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getMapa() {
+        return mapa;
+    }
+
+    public void setMapa(String mapa) {
+        this.mapa = mapa;
+    }
+
+    @XmlTransient
+    public List<Ruta> getRutaList() {
+        return rutaList;
+    }
+
+    public void setRutaList(List<Ruta> rutaList) {
+        this.rutaList = rutaList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ciudad)) {
+            return false;
+        }
+        Ciudad other = (Ciudad) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Ciudad[ id=" + id + " ]";
+    }
+    
+}
---
+++ netbeans/jpa_cities/src/java/entities/Ruta.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author micro
+ */
+@Entity
+@Table(name = "RUTA")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Ruta.findAll", query = "SELECT r FROM Ruta r")
+    , @NamedQuery(name = "Ruta.findById", query = "SELECT r FROM Ruta r WHERE r.id = :id")
+    , @NamedQuery(name = "Ruta.findByNombre", query = "SELECT r FROM Ruta r WHERE r.nombre = :nombre")
+    , @NamedQuery(name = "Ruta.findByImagen", query = "SELECT r FROM Ruta r WHERE r.imagen = :imagen")
+    , @NamedQuery(name = "Ruta.findByDescripcion", query = "SELECT r FROM Ruta r WHERE r.descripcion = :descripcion")
+    , @NamedQuery(name = "Ruta.findByLink", query = "SELECT r FROM Ruta r WHERE r.link = :link")})
+public class Ruta implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Short id;
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Column(name = "IMAGEN")
+    private String imagen;
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+    @Column(name = "LINK")
+    private String link;
+    @JoinColumn(name = "CIUDAD", referencedColumnName = "ID")
+    @ManyToOne
+    private Ciudad ciudad;
+    @OneToMany(mappedBy = "ruta")
+    private List<Punto> puntoList;
+
+    public Ruta() {
+    }
+
+    public Ruta(Short id) {
+        this.id = id;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @XmlTransient
+    public List<Punto> getPuntoList() {
+        return puntoList;
+    }
+
+    public void setPuntoList(List<Punto> puntoList) {
+        this.puntoList = puntoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ruta)) {
+            return false;
+        }
+        Ruta other = (Ruta) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Ruta[ id=" + id + " ]";
+    }
+    
+}
